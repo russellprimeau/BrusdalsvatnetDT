698db0d8cb268aba8c0eb758c978e3014ce68d54
russellprimeau <122868925+russellprimeau@users.noreply.github.com>
2023-12-17 23:52:30 -0500

    Update Dashboard.py
    Correcting syntax error in previous update.

--- Dashboard.py
+++ Dashboard.py
@@ -193,7 +193,7 @@ def hourly():
         p.xaxis.formatter = DatetimeTickFormatter(days="%Y/%m/%d", hours="%y/%m/%d %H:%M")
         st.bokeh_chart(p, use_container_width=True)
         st.write("Use the pan, zoom, save and reset buttons on the right to interact with the chart.")
-        st.markdown(#####"Have you found a bug? Or do you have an idea for how to improve the app? Log suggestions at [link](https://github.com/russellprimeau/BrusdalsvatnetDT/issues)")
+        st.markdown("##### Have you found a bug? Or do you have an idea for how to improve the app? Log suggestions at [link](https://github.com/russellprimeau/BrusdalsvatnetDT/issues)")
 
 
 def vertical():
