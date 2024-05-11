f199bbfc21e5cc243ca6e0572e298ec2144fe5de
russellprimeau <122868925+russellprimeau@users.noreply.github.com>
2024-05-11 15:05:05 +0200

    Changes to: All_time.csv ScrapeWeatherBatch.bat

--- ScrapeWeatherBatch.bat
+++ ScrapeWeatherBatch.bat
@@ -8,22 +8,22 @@ rem - conda to be in the PATH
 rem - cmd.exe to be initialized with conda init
 
 rem Define here the path to your conda installation
-set CONDAPATH=C:\Users\russelbp\AppData\Local\anaconda3
+rem set CONDAPATH=C:\Users\russelbp\AppData\Local\anaconda3
 rem Define here the name of the environment
-set ENVNAME=dfm_tools_env
+rem set ENVNAME=dfm_tools_env
 
 rem The following command activates the base environment.
-if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=%CONDAPATH%\envs\%ENVNAME%)
+rem if %ENVNAME%==base (set ENVPATH=%CONDAPATH%) else (set ENVPATH=%CONDAPATH%\envs\%ENVNAME%)
 
 rem Activate the conda environment
 rem Using call is required here, see: https://stackoverflow.com/questions/24678144/conda-environments-and-bat-files
-call %CONDAPATH%\Scripts\activate.bat %ENVPATH%
+rem call %CONDAPATH%\Scripts\activate.bat %ENVPATH%
 
 rem Run a python script in that environment
-python ScrapeWeather.py
+C:\Users\russelbp\GitHub\BrusdalsvatnetDT\venv\Scripts\python.exe ScrapeWeather.py
 
 rem Deactivate the environment
-call conda deactivate
+rem call conda deactivate
 
 rem Other approaches: if conda is directly available from the command line then the following code works.
 rem call activate someenv
