6934d91e64889df6305038dfc7a1c6a03a1f82ed
russellprimeau <122868925+russellprimeau@users.noreply.github.com>
2024-05-11 14:02:10 +0200

    Changes to: All_time.csv ScrapeWeather.py

--- ScrapeWeather.py
+++ ScrapeWeather.py
@@ -87,17 +87,13 @@ def write(df, destination):
 
         # Convert datetime objects to ISO 8601 format strings
         iso_format = '%Y-%m-%dT%H:%M:%S'
-        filtered_df.loc['Time'] = filtered_df['Time'].dt.strftime(iso_format)
+        filtered_df.loc['Time_str'] = filtered_df['Time'].dt.strftime(iso_format)
+        filtered_df = filtered_df.drop('Time', axis=1)
+        cols = ['Time_str'] + [col for col in df.columns if col != 'Time_str']
+        filtered_df = filtered_df[cols]
 
         # filtered_df = filtered_df[['Time_format'] + list(filtered_df.filter(like='Time'))]
-        print('filtered_df 2\n', filtered_df.shape, filtered_df)
-
-        # Drop the old Time column
-        # filtered_df = filtered_df.drop('Time', axis=1)
-        # cols = ['column_to_move'] + [col for col in df.columns if col != 'column_to_move']
-        #
-        # # Reindex the DataFrame with the new column order
-        # df = df[cols]
+        print('filtered_df after\n', filtered_df.shape, '\n', filtered_df)
 
         try:
             filtered_df.to_csv(destination, mode='a', index=False, header=False, sep=";", decimal=",")
